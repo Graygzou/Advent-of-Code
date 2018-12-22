e2b67ebdff9e1204dfefd39e848ad624eae3413f
Graygzou <gregoire.boiron@gmail.com>
2018-12-22 07:51:36 +0100

    Cleaned day 14 source code.

--- Day14_Chocolate_Charts/day14.lua
+++ Day14_Chocolate_Charts/day14.lua
@@ -35,8 +35,6 @@ function initCellList(initialList, elfsInitialCellScore)
   repeat
     i = i + 1   -- Regular update
 
-    print("DEBUG" .. i .. ", " .. initialList[i])
-
     -- Create the next empty node
     local nextCell = {score = initialList[i], previous=nil, next = nil}
 
@@ -65,28 +63,6 @@ function initCellList(initialList, elfsInitialCellScore)
   currentCell.next = firstCell
   firstCell.previous = currentCell
 
-  --[[
-  print("la" .. firstCell.score)
-  print("la" .. firstCell.next.score)
-  print("la" .. firstCell.next.next.score)
-  print("la" .. firstCell.next.next.next.score)
-
-  print("lu" .. firstCell.score)
-  print("lu" .. firstCell.previous.score)
-  print("lu" .. firstCell.previous.previous.score)
-  print("lu" .. firstCell.previous.previous.previous.score)
-
-  print("lo" .. currentCell.next.score)
-  print("lo" .. currentCell.next.next.score)
-  print("lo" .. currentCell.next.next.next.score)
-
-
-  print("li" .. currentCell.previous.score)
-  print("li" .. currentCell.previous.previous.score)
-  print("li" .. currentCell.previous.previous.previous.score)
-  --]]
-
-
   return firstCell, currentCell, i, elfs
 end
 
@@ -100,8 +76,6 @@ function numberToDigitsArray(number)
   repeat
     -- Create a new ref for the current digit
     table.insert(digitsArray, digitsLeft % 10)
-
-    --print(digitsLeft % 10)
 
     -- Update the digitLeft to treat
     digitsLeft = digitsLeft // 10
@@ -148,41 +122,26 @@ end
 ------------------------------------------------------------------------
 --
 ------------------------------------------------------------------------
-function addNewRecipeRef(newRecipeNumber, endingCellRef, startingCellRef, startingCellInput, furtherEqualRef)
-  local found = false
+function addNewRecipeRef(nbRecipes, newRecipeNumber, endingCellRef, startingCellRef, startingCellInput, inputStruct)
   local digits = numberToDigitsArray(newRecipeNumber)
 
   -- Iterate on the new array and create cells for the linked list
   for i = #digits,1,-1 do
 
-    --print("AAAH", furtherEqualRef.score, digits[i])
-
-    -- Check if the score added correspond to our input
-    if tonumber(furtherEqualRef.score) == tonumber(digits[i]) then
-      --print("EQUALS !")
-      -- Move on the input linked list
-      if furtherEqualRef.next == startingCellInput then
-        --print("WE DID IT !!!!")
-        found = true
-      else
-        --print("ALMOST THERE !")
-        furtherEqualRef = furtherEqualRef.next
-      end
-    else
+    if tonumber(inputStruct.node.score) ~= tonumber(digits[i]) then
       -- Reset everything...
-      furtherEqualRef = startingCellInput
+      inputStruct.node = startingCellInput
+      inputStruct.found = false
+      inputStruct.index = nbRecipes + (#digits - i) + 1
+    end
 
-      -- Test once again..
-      if tonumber(furtherEqualRef.score) == tonumber(digits[i]) then
-        --print("EQUALS !")
-        -- Move on the input linked list
-        if furtherEqualRef.next == startingCellInput then
-          --print("WE DID IT !!!!")
-          found = true
-        else
-          --print("ALMOST THERE !")
-          furtherEqualRef = furtherEqualRef.next
-        end
+    -- Check if the score (which need to be added) correspond to our input current node.
+    if tonumber(inputStruct.node.score) == tonumber(digits[i]) then
+      -- Move on the input linked list
+      if inputStruct.node.next == startingCellInput then
+        inputStruct.found = true
+      else
+        inputStruct.node = inputStruct.node.next
       end
     end
 
@@ -195,7 +154,7 @@ function addNewRecipeRef(newRecipeNumber, endingCellRef, startingCellRef, starti
     endingCellRef = newRecipe
   end
 
-  return endingCellRef, #digits, furtherEqualRef, found
+  return endingCellRef, (nbRecipes + #digits), inputStruct
 end
 
 ------------------------------------------------------------------------
@@ -208,9 +167,6 @@ function printLinkedList(nodeRef, MaxValue, threshold)
   local finalString = ""
   while l and index <= MaxValue do
     if index > tonumber(threshold) and nbValueAdded < 10 then
-      if nbValueAdded == 0 then
-        print("RESULT PART TWO : " .. index-1)
-      end
       finalString = finalString .. l.score
       nbValueAdded = nbValueAdded + 1
     end
@@ -247,7 +203,6 @@ end
 local function partOne (input, nbElfs)
   local endingCellRef = nil
   local startingCellRef = nil
-  local cells = nil
   local elfs = nil
   local nbRecipes = 0
 
@@ -255,44 +210,16 @@ local function partOne (input, nbElfs)
   -- The endingCellRef is here to allow adding more recipes.
   startingCellRef, endingCellRef, nbRecipes, elfs = initCellList({3, 7}, {3, 7})
 
-  --- DEBUG CELLS LIST ---
-  --printLinkedList(endingCellRef, 10)
-
-  -- DEBUG ELFS
-  -- [[
-  print("ELFS")
-  for i = 1, #elfs do
-    print(elfs[i].id .. ", " .. elfs[i].cell.score)
-  end
-  --]]
-
-  print("END")
-
-  -----------------------
-  -- An elf only require the cell to compute everything
-  -- (can get the previous score and know the index he's on)
-  --for i = 1,nbElfs do
-  --  table.insert(elfs, {cell = cells[i]})
-  --end
-  -----------------------
-
   -- Start the algorithm
-  while nbRecipes < (input) + 10 do
+  while nbRecipes < input + 10 do
     newRecipeNumber = 0
     -- Compute new recipes
     for i = 1, #elfs do
-      --print(elfs[i].cell.score)
       newRecipeNumber = newRecipeNumber + elfs[i].cell.score
     end
 
-    --print("DEBUG VALUE " .. newRecipeNumber)
-
     endingCellRef, nbRecipeAdded = addNewRecipe(newRecipeNumber, endingCellRef, startingCellRef)
-
     nbRecipes = nbRecipes + nbRecipeAdded
-
-    --- DEBUG CELLS LIST ---
-    --printLinkedList(endingCellRef, 20)
 
     -- For each elfs, moved in the scoreboard and pick a new current recipe
     for i = 1, #elfs do
@@ -301,8 +228,6 @@ local function partOne (input, nbElfs)
       -- Compute the number of steps
       nbSteps = tempCell.score + 1
 
-      --print("ELF " .. i .. ", STEPS = " .. nbSteps)
-
       local counter = 0
       repeat
         tempCell = tempCell.next
@@ -310,7 +235,6 @@ local function partOne (input, nbElfs)
       until counter >= nbSteps
 
       elfs[i].cell = tempCell
-      --print("ELF " .. i .. ", NEW SCORE = " .. elfs[i].cell.score)
     end
   end
 
@@ -333,7 +257,6 @@ end
 local function partTwo (input, nbElfs)
   local endingCellRef = nil
   local startingCellRef = nil
-  local cells = nil
   local elfs = nil
 
   local nbRecipes = 0
@@ -341,24 +264,12 @@ local function partTwo (input, nbElfs)
   -- Create a circular linked list to make easier the loop between the last and the first cell.
   -- The endingCellRef is here to allow adding more recipes.
   startingCellRef, endingCellRef, nbRecipes, elfs = initCellList({3, 7}, {3, 7})
-
-  temp = stringToDigitsArray(input)
-
-  print("DEBUG")
-  for i = 1,#temp do
-    print(temp[i])
-  end
-
   startingCellInput, endingCellInput = initCellList(stringToDigitsArray(input), nil)
 
-  local finalString = printLinkedList(startingCellInput, 6, 0)
-
   -- Start the algorithm
-  local furtherEqualRef = startingCellInput
+  local inputStruct = {node=startingCellInput, found=false, index=nbRecipes}
 
-  found = false
-
-  while not found and nbRecipes < 50000000 do
+  while (not inputStruct.found) and nbRecipes < 50000000 do
 
     newRecipeNumber = 0
     -- Compute new recipes
@@ -366,10 +277,7 @@ local function partTwo (input, nbElfs)
       newRecipeNumber = newRecipeNumber + elfs[i].cell.score
     end
 
-    endingCellRef, nbRecipeAdded, furtherEqualRef, found = addNewRecipeRef(newRecipeNumber, endingCellRef, startingCellRef, startingCellInput, furtherEqualRef)
-    nbRecipes = nbRecipes + nbRecipeAdded
-
-    --print("TEMPFOUND = ", found)
+    endingCellRef, nbRecipes, inputStruct = addNewRecipeRef(nbRecipes, newRecipeNumber, endingCellRef, startingCellRef, startingCellInput, inputStruct)
 
     -- For each elfs, moved in the scoreboard and pick a new current recipe
     for i = 1, #elfs do
@@ -378,8 +286,6 @@ local function partTwo (input, nbElfs)
       -- Compute the number of steps
       nbSteps = tempCell.score + 1
 
-      --print("ELF " .. i .. ", STEPS = " .. nbSteps)
-
       local counter = 0
       repeat
         tempCell = tempCell.next
@@ -387,26 +293,14 @@ local function partTwo (input, nbElfs)
       until counter >= nbSteps
 
       elfs[i].cell = tempCell
-      --print("ELF " .. i .. ", NEW SCORE = " .. elfs[i].cell.score)
     end
   end
 
-  --finalString = printLinkedList(startingCellRef, 19, 9)
-
-  print("@@@@@@@@@@@@@@@@@@@@@@@")
-  print(nbRecipes)
-  print(endingCellRef.next == startingCellInput)
-  print(endingCellRef.next.score)
-  print(startingCellInput.score)
-  print("@@@@@@@@@@@@@@@@@@@@@@@")
-
-  printLinkedListBack(endingCellRef, 10)
-
   print("================================================================")
-  print("FINAL PART TWO : " .. nbRecipes - #input)
+  print("FINAL PART TWO : " .. inputStruct.index)
   print("================================================================")
 
-  return nbRecipes - #input;
+  return inputStruct.index;
 end
 
 
@@ -414,29 +308,25 @@ end
 -- Main - Main function
 --#################################################################
 function day14Main (filename)
-  -- Tests
-  --local puzzleInput = 9
-  --local puzzleInput = 5
-  --local puzzleInput = 18
-  --local puzzleInput = 2018
+  local nbElfs = 2
 
-  -- Final
-  --local puzzleInput = 074501
-  --local nbElfs = 2
+  local puzzleInput1 = 9
+  local puzzleInput2 = "51589"
 
-  --local resultPartOne = partOne(puzzleInput, nbElfs)
+  --local puzzleInput1 = 5
+  --local puzzleInput2 = "01245"
 
+  --local puzzleInput1 = 18
+  --local puzzleInput2 = "92510"
 
-  -- Tests
-  --local puzzleInput = "51589"
-  --local puzzleInput = "01245"
-  --local puzzleInput = "92510"
-  --local puzzleInput = "59414"
+  --local puzzleInput1 = 2018
+  --local puzzleInput2 = "59414"
 
-  -- Final
-  local puzzleInput = "074501"
+  --local puzzleInput1 = 074501
+  --local puzzleInput2 = "074501"
 
-  local resultPartTwo = partTwo(puzzleInput, nbElfs)
+  local resultPartOne = partOne(puzzleInput1, nbElfs)
+  local resultPartTwo = partTwo(puzzleInput2, nbElfs)
 
   -- Launch and print the final result
   print("Result part one :", resultPartOne);
